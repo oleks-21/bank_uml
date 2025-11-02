6c6a546a1331ad7257ad78580c5490b349aab90a
Oleksandr Stavnichuk <oleks@Oleksandrs-MacBook-Pro.local>
2025-11-02 10:06:44 -0500

    fixes to port

--- backend/server.js
+++ backend/server.js
@@ -46,4 +46,4 @@ app.get("/users", (req, res) => {
   });
 });
 
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`✅ Server running on port ${PORT}`));
